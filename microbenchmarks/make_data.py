443e1d7fccd25e07f30ed4c7083943c056a19bb8
Shoumik Palkar <shoumik@cs.stanford.edu>
2017-04-25 15:58:08 -0700

    Data generation script for numbers fix

--- microbenchmarks/make_data.py
+++ microbenchmarks/make_data.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #
 # Creates fake numeric CSV data.
 #
@@ -9,7 +11,7 @@ import argparse
 
 def generate_data(rows, columns, maxint=1000000, filename="data/numbers.csv"):
     numbers = np.random.rand(rows,columns) * maxint
-    np.savetxt("data/numbers.csv", numbers, fmt="%d")
+    np.savetxt("data/numbers.csv", numbers, fmt="%d", delimiter=",")
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Number of lines in the file.')
