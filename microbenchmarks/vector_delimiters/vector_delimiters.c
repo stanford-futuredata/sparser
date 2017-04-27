f5ea6b58cf4e2857d8a53d28fdbf054e8fc1fc87
Shoumik Palkar <shoumik@cs.stanford.edu>
2017-04-26 19:01:22 -0700

    Work with non-multiples of 64 (with a small bug)

--- microbenchmarks/vector_delimiters/vector_delimiters.c
+++ microbenchmarks/vector_delimiters/vector_delimiters.c
@@ -10,7 +10,7 @@
 
 #include "common.h"
 
-const char *test_string = "United,Airlines,An airline based in San Francisco CA,Flig1423123";
+const char *test_string = "United,Airlines,An airline based in San Francisco CA,Flig1423123,2423423123123,2442342,1231245";
 const int iterations = 1000000;
 
 double measure_strsep() {
@@ -39,13 +39,21 @@ double measure_strsep() {
 double measure_vector() {
     int count = 0;
 
-    unsigned length = strlen(test_string) * sizeof(char);
+    // Length of the string in bytes.
+    unsigned length = strlen(test_string);
     __m256i delimiter = _mm256_set1_epi8(',');
+
+    // All ones.
+    __m256i ones = _mm256_cmpeq_epi8(delimiter, delimiter);
 
     time_start();
     for (int i = 0; i < iterations; i++) {
-        for (int i = 0; i + 32 <= length; i += 32) {
+        for (int i = 0; i <= length; i += 32) {
             __m256i word = _mm256_load_si256((__m256i const *)(test_string + i));
+            // Last iteration - mask out bytes past the end of the input
+            if (i + 32 > length) {
+                // TODO - mask out unused "out of bounds" bytes.
+            }
             __m256i mask = _mm256_cmpeq_epi8(word, delimiter);
             int imask = _mm256_movemask_epi8(mask);
             while (imask) {
@@ -54,8 +62,8 @@ double measure_vector() {
                 imask &= ~(1 << idx);
             }
         }
-        count++;
         // for the token after the last delimiter.
+        count++;
     }
     double time = time_stop();
 
@@ -66,6 +74,9 @@ double measure_vector() {
 }
 
 int main() {
+
+    printf("String length: %u\n", strlen(test_string));
+
     // Compare exact match without parsing.
     double c = measure_strsep();
     double d = measure_vector();
