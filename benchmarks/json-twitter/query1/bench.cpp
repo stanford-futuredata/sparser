a4844fc09555576e70f49a222e2960046171f386
Shoumik Palkar <shoumik@cs.stanford.edu>
2017-08-17 23:07:19 -0700

    clang format some files

--- benchmarks/json-twitter/query1/bench.cpp
+++ benchmarks/json-twitter/query1/bench.cpp
@@ -1,17 +1,17 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <time.h>
 
 #include <string.h>
 
-#include <immintrin.h>
 #include <arpa/inet.h>
+#include <immintrin.h>
 
 #include "rapidjson/document.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
 #include "rapidjson/error/en.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
 
 #include "common.h"
 #include "sparser.h"
@@ -22,119 +22,119 @@ using namespace rapidjson;
 const char *TEXT = "Putin";
 const char *USER_NAME = "LaVerne";
 
-// Callback - parses using RapidJSON and returns true if passed *all* predicates.
+// Callback - parses using RapidJSON and returns true if passed *all*
+// predicates.
 bool rapidjson_parse(const char *line);
 
-/** Uses sparser and RapidJSON to count the number of records matching the predicate.
+/** Uses sparser and RapidJSON to count the number of records matching the
+ * predicate.
  *
  * @param filename the data to check
- * @param query a search query. If this is NULL, TEXT is used. At most four bytes from this query
- * will be used.
+ * @param query a search query. If this is NULL, TEXT is used. At most four
+ * bytes from this query will be used.
  *
  * @return the running time.
  */
 double baseline(const char *filename, const char *query) {
-    // Read in the data into a buffer.
-    char *raw = NULL;
-    long length = read_all(filename, &raw);
+  // Read in the data into a buffer.
+  char *raw = NULL;
+  long length = read_all(filename, &raw);
 
-    bench_timer_t s = time_start();
+  bench_timer_t s = time_start();
 
-    int query_length;
-    if (!query) {
-        query = TEXT;
-        query_length = 4;
-    } else {
-        query_length = strlen(query);
-        if (query_length > 4) {
-            query_length = 4;
-        }
+  int query_length;
+  if (!query) {
+    query = TEXT;
+    query_length = 4;
+  } else {
+    query_length = strlen(query);
+    if (query_length > 4) {
+      query_length = 4;
     }
+  }
 
-    sparser_stats_t *stats = sparser_search_single(raw,
-            length,
-            query,
-            4,
-            rapidjson_parse);
+  sparser_stats_t *stats =
+      sparser_search_single(raw, length, query, 4, rapidjson_parse);
 
-    assert(stats);
+  assert(stats);
 
-    double parse_time = time_stop(s);
+  double parse_time = time_stop(s);
 
-    printf("%s\n", sparser_format_stats(stats));
-    printf("Runtime: %f seconds\n", parse_time);
+  printf("%s\n", sparser_format_stats(stats));
+  printf("Runtime: %f seconds\n", parse_time);
 
-    free(raw);
-    free(stats);
+  free(raw);
+  free(stats);
 
-    return parse_time;
+  return parse_time;
 }
 
-// Performs a parse of the query using RapidJSON. Returns true if all the predicates match.
+// Performs a parse of the query using RapidJSON. Returns true if all the
+// predicates match.
 bool rapidjson_parse(const char *line) {
-    Document d;
-    d.Parse(line);
-    if (d.HasParseError()) {
-        fprintf(stderr, "\nError(offset %u): %s\n", 
-                (unsigned)d.GetErrorOffset(),
-                GetParseError_En(d.GetParseError()));
-        return false;
-    }
+  Document d;
+  d.Parse(line);
+  if (d.HasParseError()) {
+    fprintf(stderr, "\nError(offset %u): %s\n", (unsigned)d.GetErrorOffset(),
+            GetParseError_En(d.GetParseError()));
+    return false;
+  }
 
-    Value::ConstMemberIterator itr = d.FindMember("text");
-    if (itr == d.MemberEnd()) {
-        // The field wasn't found.
-        return false;
-    }
-    if (strstr(itr->value.GetString(), TEXT) == NULL) {
-        return false;
-    }
+  Value::ConstMemberIterator itr = d.FindMember("text");
+  if (itr == d.MemberEnd()) {
+    // The field wasn't found.
+    return false;
+  }
+  if (strstr(itr->value.GetString(), TEXT) == NULL) {
+    return false;
+  }
 
-    itr = d.FindMember("user");
-    if (itr == d.MemberEnd()) {
-        return false;
-    }
+  itr = d.FindMember("user");
+  if (itr == d.MemberEnd()) {
+    return false;
+  }
 
-    auto user = itr->value.GetObject();
-    itr = user.FindMember("name");
-    if (itr == d.MemberEnd()) {
-        return false;
-    }
-    
-    if (strcmp(itr->value.GetString(), USER_NAME) != 0) {
-        return false;
-    }
+  auto user = itr->value.GetObject();
+  itr = user.FindMember("name");
+  if (itr == d.MemberEnd()) {
+    return false;
+  }
 
-    return true;
+  if (strcmp(itr->value.GetString(), USER_NAME) != 0) {
+    return false;
+  }
+
+  return true;
 }
 
 /// JSON Parser version.
 double baseline_rapidjson(const char *filename) {
-    char *data, *line;
-    size_t bytes = read_all(filename, &data);
-    int doc_index = 1;
-    int matching = 0;
+  char *data, *line;
+  size_t bytes = read_all(filename, &data);
+  int doc_index = 1;
+  int matching = 0;
 
-    bench_timer_t s = time_start();
+  bench_timer_t s = time_start();
 
-    while ((line = strsep(&data, "\n")) != NULL) {
-        if (rapidjson_parse(line)) {
-            matching++;
-        }
-        doc_index++;
+  while ((line = strsep(&data, "\n")) != NULL) {
+    if (rapidjson_parse(line)) {
+      matching++;
     }
+    doc_index++;
+  }
 
-    double elapsed = time_stop(s);
-    printf("Passing Elements: %d of %d records (%.3f seconds)\n", matching, doc_index, elapsed);
-    return elapsed;
+  double elapsed = time_stop(s);
+  printf("Passing Elements: %d of %d records (%.3f seconds)\n", matching,
+         doc_index, elapsed);
+  return elapsed;
 }
 
 int main() {
-    const char *filename = path_for_data("tweets.json");
-    double a = baseline(filename, TEXT);
-    double b = baseline_rapidjson(filename);
+  const char *filename = path_for_data("tweets.json");
+  double a = baseline(filename, TEXT);
+  double b = baseline_rapidjson(filename);
 
-    printf("Speedup: %f\n", b / a);
+  printf("Speedup: %f\n", b / a);
 
-    return 0;
+  return 0;
 }
--- common/sparser.h
+++ common/sparser.h
@@ -9,20 +9,21 @@
 // Size of the register we use.
 const int VECSZ = 32;
 
-/// Takes a register containing a search token and a base address, and searches the base address for the search token.
-typedef int (* sparser_searchfunc_t)(__m256i, const char *);
+/// Takes a register containing a search token and a base address, and searches
+/// the base address for the search token.
+typedef int (*sparser_searchfunc_t)(__m256i, const char *);
 
 // The callback fro the single parse function.
-typedef bool (* sparser_callback_t)(const char *input);
+typedef bool (*sparser_callback_t)(const char *input);
 
 typedef struct sparser_stats_ {
-    long total_matches;
-    long sparser_passed;
-    long callback_passed;
-    long bytes_seeked_forward;
-    long bytes_seeked_backward;
-    double fraction_passed_correct;
-    double fraction_passed_incorrect;
+  long total_matches;
+  long sparser_passed;
+  long callback_passed;
+  long bytes_seeked_forward;
+  long bytes_seeked_backward;
+  double fraction_passed_correct;
+  double fraction_passed_incorrect;
 } sparser_stats_t;
 
 /** Search for an 8-bit search string.
@@ -33,17 +34,17 @@ typedef struct sparser_stats_ {
  * @return the number of matches found.
  */
 int search_epi8(__m256i reg, const char *base) {
-    int count = 0;
-    __m256i val = _mm256_loadu_si256((__m256i const *)(base));
-    unsigned mask = _mm256_movemask_epi8(_mm256_cmpeq_epi8(reg, val));
+  int count = 0;
+  __m256i val = _mm256_loadu_si256((__m256i const *)(base));
+  unsigned mask = _mm256_movemask_epi8(_mm256_cmpeq_epi8(reg, val));
 
-    // TODO popcnt better here, since we're not returning positions?
-    while (mask) {
-        int index = ffs(mask) - 1;
-        mask &= ~(1 << index);
-        count++;
-    }
-    return count;
+  // TODO popcnt better here, since we're not returning positions?
+  while (mask) {
+    int index = ffs(mask) - 1;
+    mask &= ~(1 << index);
+    count++;
+  }
+  return count;
 }
 
 /** Search for an 16-bit search string.
@@ -54,17 +55,17 @@ int search_epi8(__m256i reg, const char *base) {
  * @return the number of matches found.
  */
 int search_epi16(__m256i reg, const char *base) {
-    int count = 0;
-    __m256i val = _mm256_loadu_si256((__m256i const *)(base));
-    unsigned mask = _mm256_movemask_epi8(_mm256_cmpeq_epi16(reg, val));
-    mask &= 0xf0f0f0f0f0;
+  int count = 0;
+  __m256i val = _mm256_loadu_si256((__m256i const *)(base));
+  unsigned mask = _mm256_movemask_epi8(_mm256_cmpeq_epi16(reg, val));
+  mask &= 0xf0f0f0f0f0;
 
-    while (mask) {
-        int index = ffs(mask) - 1;
-        mask &= ~(1 << index);
-        count++;
-    }
-    return count;
+  while (mask) {
+    int index = ffs(mask) - 1;
+    mask &= ~(1 << index);
+    count++;
+  }
+  return count;
 }
 
 /** Search for an 32-bit search string.
@@ -75,23 +76,24 @@ int search_epi16(__m256i reg, const char *base) {
  * @return the number of matches found.
  */
 int search_epi32(__m256i reg, const char *base) {
-    int count = 0;
-    __m256i val = _mm256_loadu_si256((__m256i const *)(base));
-    unsigned mask = _mm256_movemask_epi8(_mm256_cmpeq_epi32(reg, val));
-    mask &= 0x11111111;
+  int count = 0;
+  __m256i val = _mm256_loadu_si256((__m256i const *)(base));
+  unsigned mask = _mm256_movemask_epi8(_mm256_cmpeq_epi32(reg, val));
+  mask &= 0x11111111;
 
-    while (mask) {
-        int index = ffs(mask) - 1;
-        mask &= ~(1 << index);
-        count++;
-    }
-    return count;
+  while (mask) {
+    int index = ffs(mask) - 1;
+    mask &= ~(1 << index);
+    count++;
+  }
+  return count;
 }
 
-/* Performs the sparser search given a single search query and a buffer. 
+/* Performs the sparser search given a single search query and a buffer.
  *
- * This performs a simple sparser search given the query and input buffer. It only searches for one occurance of the query
- * string in each record. Records are assumed to be delimited by newline.
+ * This performs a simple sparser search given the query and input buffer. It
+ * only searches for one occurance of the query string in each record. Records
+ * are assumed to be delimited by newline.
  *
  * @param input the buffer to search
  * @param query the query to look for
@@ -100,115 +102,110 @@ int search_epi32(__m256i reg, const char *base) {
  *
  * @return statistics about the run.
  * */
-sparser_stats_t *
-sparser_search_single(char *input,
-        long length,
-        const char *query,
-        long qlength,
-        sparser_callback_t callback) {
+sparser_stats_t *sparser_search_single(char *input, long length,
+                                       const char *query, long qlength,
+                                       sparser_callback_t callback) {
 
-    sparser_searchfunc_t searchfunc;
-    __m256i reg;
+  sparser_searchfunc_t searchfunc;
+  __m256i reg;
 
-    sparser_stats_t stats;
-    memset(&stats, 0, sizeof(stats));
+  sparser_stats_t stats;
+  memset(&stats, 0, sizeof(stats));
 
-    switch(qlength) {
-        case 1: {
-            searchfunc = search_epi8;
-            uint8_t x = *((uint8_t *)query);
-            reg = _mm256_set1_epi8(x);
-            break;
-                }
-        case 2: {
-            searchfunc = search_epi16;
-            uint16_t x = *((uint16_t *)query);
-            reg = _mm256_set1_epi16(x);
-            break;
-                }
-        case 4: {
-            searchfunc = search_epi32;
-            uint32_t x = *((uint32_t *)query);
-            reg = _mm256_set1_epi32(x);
-            break;
-                }
-        default: {
-            return NULL;
-                 }
+  switch (qlength) {
+  case 1: {
+    searchfunc = search_epi8;
+    uint8_t x = *((uint8_t *)query);
+    reg = _mm256_set1_epi8(x);
+    break;
+  }
+  case 2: {
+    searchfunc = search_epi16;
+    uint16_t x = *((uint16_t *)query);
+    reg = _mm256_set1_epi16(x);
+    break;
+  }
+  case 4: {
+    searchfunc = search_epi32;
+    uint32_t x = *((uint32_t *)query);
+    reg = _mm256_set1_epi32(x);
+    break;
+  }
+  default: { return NULL; }
+  }
+
+  int shifts = qlength;
+  for (long i = 0; i < length - VECSZ - shifts; i += VECSZ) {
+
+    long matches = 0;
+    for (int j = 0; j < shifts; j++) {
+      matches += searchfunc(reg, input + i + j);
     }
 
-    int shifts = qlength;
-    for (long i = 0; i < length - VECSZ - shifts; i += VECSZ) {
+    stats.total_matches += matches;
 
-        long matches = 0;
-        for (int j = 0; j < shifts; j++) {
-            matches += searchfunc(reg, input + i + j); 
-        }
+    // Found something! Seek back to the beginning of the token and invoke the
+    // callback..
+    if (matches > 0) {
 
-        stats.total_matches += matches;
+      stats.sparser_passed++;
 
-        // Found something! Seek back to the beginning of the token and invoke the callback..
-        if (matches > 0) {
+      long record_end = i;
+      for (; record_end < length && input[record_end] != '\n'; record_end++)
+        ;
+      input[record_end] = '\0';
 
-            stats.sparser_passed++;
+      stats.bytes_seeked_forward += (record_end - i);
 
-            long record_end = i;
-            for (; record_end < length && input[record_end] != '\n'; record_end++);
-            input[record_end] = '\0';
+      // Seek back to the previous newline so we can pass the full record to the
+      // parser.
+      long record_start = i;
+      for (; record_start > 0 && input[record_start] != '\0' &&
+             input[record_start] != '\n';
+           record_start--)
+        ;
 
-            stats.bytes_seeked_forward += (record_end - i);
+      if (record_start != 0) {
+        input[record_start] = '\0';
+        record_start++;
+      }
 
-            // Seek back to the previous newline so we can pass the full record to the parser.
-            long record_start = i;
-            for (; record_start > 0 && input[record_start] != '\0' &&
-                    input[record_start] != '\n';
-                    record_start--);
+      stats.bytes_seeked_backward += (i - record_start);
 
-            if (record_start != 0) {
-                input[record_start] = '\0';
-                record_start++;
-            }
-
-            stats.bytes_seeked_backward += (i - record_start);
-
-            if (callback(input + record_start)) {
-                stats.callback_passed++;
-                // TODO do something here.
-            }
-            i = record_end + 1 - VECSZ;
-        }
+      if (callback(input + record_start)) {
+        stats.callback_passed++;
+        // TODO do something here.
+      }
+      i = record_end + 1 - VECSZ;
     }
+  }
 
-    if (stats.sparser_passed > 0) {
-        stats.fraction_passed_correct = (double)stats.callback_passed / (double)stats.sparser_passed;
-        stats.fraction_passed_incorrect = 1.0 - stats.fraction_passed_correct;
-    }
+  if (stats.sparser_passed > 0) {
+    stats.fraction_passed_correct =
+        (double)stats.callback_passed / (double)stats.sparser_passed;
+    stats.fraction_passed_incorrect = 1.0 - stats.fraction_passed_correct;
+  }
 
-    sparser_stats_t *ret = (sparser_stats_t *)malloc(sizeof(sparser_stats_t));
-    memcpy(ret, &stats, sizeof(stats));
+  sparser_stats_t *ret = (sparser_stats_t *)malloc(sizeof(sparser_stats_t));
+  memcpy(ret, &stats, sizeof(stats));
 
-    return ret;
+  return ret;
 }
 
 static char *sparser_format_stats(sparser_stats_t *stats) {
-    static char buf[8192];
+  static char buf[8192];
 
-    snprintf(buf, sizeof(buf),"Query matches: %ld\n\
+  snprintf(buf, sizeof(buf), "Query matches: %ld\n\
 Sparser Passed Records: %ld\n\
 Callback PassedRecords: %ld\n\
 Bytes Seeked Forward: %ld\n\
 Bytes Seeked Backward: %ld\n\
 Fraction Passed Correctly: %f\n\
-Fraction False Positives: %f",
-            stats->total_matches,
-            stats->sparser_passed,
-            stats->callback_passed,
-            stats->bytes_seeked_forward,
-            stats->bytes_seeked_backward,
-            stats->fraction_passed_correct,
-            stats->fraction_passed_incorrect);
-    return buf;
-
+Fraction False Positives: %f", stats->total_matches,
+           stats->sparser_passed, stats->callback_passed,
+           stats->bytes_seeked_forward, stats->bytes_seeked_backward,
+           stats->fraction_passed_correct, stats->fraction_passed_incorrect);
+  return buf;
 }
 
 #endif
