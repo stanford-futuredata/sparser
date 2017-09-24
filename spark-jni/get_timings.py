9f26e440763548c32b9dc2c9be115c29b415e6d5
Firas Abuzaid <fabuzaid21@gmail.com>
2017-09-24 18:50:37 +0000

    Edited get_timings.py

--- spark-jni/get_timings.py
+++ spark-jni/get_timings.py
@@ -4,45 +4,56 @@ import sys
 import re
 import numpy as np
 
-query_dir = sys.argv[1]
+query_dir = sys.argv[1] + '/'
 
-with open(query_dir + 'read-only-hdfs.txt') as read_only_f:
-    read_only_lines = read_only_f.read()
-    read_only_timings = [
-        float(x) for x in re.findall('Total Job Time: (.*)', read_only_lines)
-    ]
-    # Skip the first run, ignore JVM warmup time
-    read_only_timings = read_only_timings[1:6]
+QUERIES = [('twitter', 4), ('zakir', 10)]
 
-with open('timings.csv', 'w') as w:
-    print >> w, 'query,spark_time,sparser_time'
-    print >> w, 'read-only,' + str(min(read_only_timings)) + ',,'
-    for query in xrange(1, 9):
-        query_str = query_dir + str(query)
-        with open(query_str + '-spark-hdfs.txt') as spark_f, open(
-                query_str + '-sparser-hdfs.txt') as sparser_f:
-            spark_lines, sparser_lines = spark_f.read(), sparser_f.read()
-
-            #read_hdfs_timings = [float(x) for x in re.findall('Read time: (.*)', lines)]
-            #sparser_timings = [float(x) for x in re.findall('Total Runtime: (.*) seconds', lines)]
-            #hdfs_sparser_timings = np.array(read_hdfs_timings) + np.array(sparser_timings)
-            #cpp_timings = [float(x) for x in re.findall('Total Time in C\+\+: (.*)', lines)]
-
-            spark_timings = [
+for query_dataset, num_queries in QUERIES:
+    with open(query_dataset + '_spark_experiments.csv', 'w') as w:
+        print >> w, 'query,spark_time,spark_std_dev,sparser_time,sparser_std_dev,query_only_time,query_only_std_dev'
+        with open(query_dir + query_dataset +
+                  '-read-only-hdfs.txt') as read_only_f:
+            read_only_lines = read_only_f.read()
+            read_only_timings = [
                 float(x)
-                for x in re.findall('Total Job Time: (.*)', spark_lines)
-            ]
-            sparser_timings = [
-                float(x)
-                for x in re.findall('Total Job Time: (.*)', sparser_lines)
+                for x in re.findall('Total Job Time: (.*)', read_only_lines)
             ]
             # Skip the first run, ignore JVM warmup time
-            spark_timings = spark_timings[1:6]
-            sparser_timings = sparser_timings[1:6]
-	    print >> w, '%s,%f,%f' % (query_str, min(spark_timings), min(sparser_timings))
+            read_only_timings = read_only_timings[1:6]
 
-        #assert (len(read_hdfs_timings) == len(sparser_timings) == len(cpp_timings)
-        #        == len(spark_timings))
-        #for a, b, c, d, e in zip(read_hdfs_timings, sparser_timings,
-        #                         hdfs_sparser_timings, cpp_timings, spark_timings):
-        #    print >> w, '%f,%f,%f,%f,%f' % (a, b, c, d, e)
+            print >> w, 'disk load time,%f,%f,,,,' % (np.mean(read_only_timings),
+                                                 np.std(read_only_timings))
+
+            for query in xrange(1, num_queries + 1):
+                query_str = query_dir + query_dataset + str(query)
+                with open(query_str + '-spark-hdfs.txt') as spark_f, open(
+                        query_str + '-sparser-hdfs.txt') as sparser_f, open(
+                            query_str +
+                            '-query-only-hdfs.txt') as query_only_f:
+                    spark_lines, sparser_lines, query_only_lines = spark_f.read(
+                    ), sparser_f.read(), query_only_f.read()
+
+                    spark_timings = [
+                        float(x)
+                        for x in re.findall('Total Job Time: (.*)',
+                                            spark_lines)
+                    ]
+                    sparser_timings = [
+                        float(x)
+                        for x in re.findall('Total Job Time: (.*)',
+                                            sparser_lines)
+                    ]
+                    query_only_timings = [
+                        float(x)
+                        for x in re.findall('Query Time: (.*)',
+                                            query_only_lines)
+                    ]
+                    # Skip the first run, ignore JVM warmup time
+                    spark_timings = spark_timings[1:6]
+                    sparser_timings = sparser_timings[1:6]
+                    query_only_timings = query_only_timings[1:6]
+                    print >> w, '%d,%f,%f,%f,%f,%f,%f' % (
+                        query, np.mean(spark_timings),
+                        np.std(spark_timings), np.mean(sparser_timings),
+                        np.std(sparser_timings), np.mean(query_only_timings),
+                        np.std(sparser_timings))
