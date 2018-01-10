d5d70bf79c483bddd10008fab83177e835dc8a0e
Shoumik Palkar <shoumik@cs.stanford.edu>
2018-01-10 11:13:46 -0800

    More work on calibration function

--- sparser-rust/src/lib.rs
+++ sparser-rust/src/lib.rs
@@ -1,9 +1,14 @@
 extern crate memchr;
-extern crate bit_vec;
+extern crate memmem;
 
 extern crate libc;
+extern crate time;
 
-use libc::{c_char, c_void, c_int};
+extern crate bit_vec;
+
+use libc::{c_uchar, c_void, c_int};
+
+use memmem::Searcher;
 
 /// The maximum word length (generally the size in bytes of a `long` in C).
 const MAX_WORD_LENGTH: usize = 8;
@@ -14,33 +19,17 @@ const MAX_CANDIDATES: usize= 64;
 /// The maximum number of samples to evaluate during calibration.
 const MAX_SAMPLES: usize = 64;
 
-/// A callback into a full parser.
-type ParserCallbackFn = fn(*mut c_char, *mut c_void) -> c_int;
-
-/// A sparser query, which encapsulates search tokens. Currently, this only
-/// supports conjunctions of tokens, though eventually we want to support
-/// disjunctions as well.
+/// A callback into a full parser. This function takes a C string as a pointer and an arbitrary
+/// context object, and returns 0 if the callback successfully passed the record and 1 if the
+/// callback failed (i.e., received a NULL record or did not pass the record).
 /// 
-/// TODO maybe this should be called a `Schedule` instead?
-struct SparserQuery {
-    queries: Vec<String>,
-}
-
-impl SparserQuery {
-    fn new() -> SparserQuery {
-        SparserQuery {
-            queries: vec![]
-        }
-    }
-
-    fn add_query<S>(&mut self, v: S) where S: Into<String> {
-        self.queries.push(v.into());
-    }
-}
+/// The context generally encapsulates the query to execute on the parsed record (e.g., a set of
+/// selections or projections).
+type ParserCallbackFn = fn(*const c_uchar, *mut c_void) -> c_int;
 
 /// Returns all substrings of `s` of up to length `max` (and of at least length 2).
 /// The returned substrings all fit exactly within a word, i.e., are 2, 4, or 8 bytes long.
-fn all_substrings<'a>(s: &'a str, max: usize) -> Vec<&'a str> {
+fn all_substrings(s: &str, max: usize) -> Vec<&str> {
     let mut substrings = vec![];
     let mut max = std::cmp::min(s.len(), max);
 
@@ -85,15 +74,21 @@ fn generate_candidates(predicates: &Vec<String>) -> Vec<&str> {
     candidates
 }
 
-/// Given an input dataset `sample` and a set of `predicates`, 
-fn calibrate(sample: &mut [u8], predicates: Vec<String>) {
+/// Runs the sparser scheduler and returns a `SparserQuery`, which is interpreted by the engine
+/// during execution.
+/// 
+/// TODO: Bunch of work left here!
+pub fn calibrate(sample: &mut [u8], predicates: Vec<String>, parser_callback: ParserCallbackFn) {
     let candidates = generate_candidates(&predicates);
 
     // The estimated callback cost.
-    let mut callback_cost = 0;
+    let mut callback_cost = time::Duration::nanoseconds(0);
     let mut records_processed = 0;
 
     // Tracks false positives across samples.
+    // If bit false_positives[i][j] is set, then the candidate pre-filter `i` returned a false
+    // positive for sample `j`. A false positive is defined as a pre-filter which passes a record,
+    // but the callback fails the record.
     let mut false_positives = vec![bit_vec::BitVec::from_elem(MAX_SAMPLES, false); candidates.len()];
 
     // The index of the record currently being processed as an offset from `sample`.
@@ -107,8 +102,12 @@ fn calibrate(sample: &mut [u8], predicates: Vec<String>) {
             let mut found = bit_vec::BitVec::from_elem(candidates.len(), false);
 
             for (i, candidate) in candidates.iter().enumerate() {
-                // TODO Search for the predicates in this record using memmem or something.
-                if false {
+                // TODO! This part will change based on the *kind* of candidate (i.e., how the
+                // candidate is applied).  This is the biggest change we want to make for VLDB;
+                // instead of just grepping for some terms, we need some kind of interpretation
+                // engine that can handle different kinds of pre-filters.
+                let searcher = memmem::TwoWaySearcher::new(candidate.as_bytes());
+                if let Some(_) = searcher.search_in(sample.get(base..base + endpos).unwrap()) {
                     false_positives[i].set(records_processed, true);
                     found.set(i, true);
                 }
@@ -117,8 +116,14 @@ fn calibrate(sample: &mut [u8], predicates: Vec<String>) {
             // If all the candidates were found, we need to run the full parser to check
             // if they're all false positives.
             if found.all() {
-                // TODO call the callback here and time it!
-                if true { 
+                let c_sample = unsafe { sample.as_mut_ptr().offset(base as isize) };
+
+                let start = time::PreciseTime::now();
+                let passed = parser_callback(c_sample, std::ptr::null_mut()) as i32;
+                let end = time::PreciseTime::now();
+                callback_cost = callback_cost + start.to(end);
+
+                if passed != 0 { 
                     // the callback passed too, so these aren't false positives!
                     for i in 0..candidates.len() {
                         false_positives[i].set(records_processed, false);
